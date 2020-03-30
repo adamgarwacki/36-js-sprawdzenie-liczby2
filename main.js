72aee913c955e9dcb39e53e30694de2580f615f9
Adam Garwacki <adam.garwacki@gmail.com>
2020-03-30 10:06:23 +0200

    Stworzenie pliku 'main.js'. Stworzenie funkcji testującej wiele warunków.

---
+++ main.js
@@ -0,0 +1,11 @@
+let sprawdzanieLiczby2 = (num1, num2) => {
+    if (num1 == 20 || num2 == 20 || num1 + num2 <= 20) {
+        return true
+    } else {
+        return false;
+    }
+}
+
+// console.log(sprawdzanieLiczby2(10, 12));
+// console.log(sprawdzanieLiczby2(5, 3));
+// console.log(sprawdzanieLiczby2(0, 20));
